ff2290067e5fb59af8bfae10962713e09ada1698
Lucas Luiz <lucas.luiz10498@gmail.com>
2022-10-06 14:32:02 -0300

    versao com layout inicial

---
+++ src/PageChoseMovie.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+export default function PageChoseMovie(){
+    return(
+        <Movie></Movie>
+
+    )
+}
+
+const Movie = styled.div`
+ width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+    img{
+        
+    }
+`
--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import styled from "styled-components"
+import PageChoseMovie from "./PageChoseMovie"
 export default function App() {
     return (
-        <Container />
+        <Container >
+            <Menu><p>CINEFLIX</p></Menu>
+            <Chose><p>Selecione o filme</p></Chose>
+        </Container>
     )
 }
 const Container = styled.div`
-  background-color: #FB6B6B;
+  background-color: #FFFFFF;
   width: 100vw;
   min-height: 100vh;
   display: flex;
@@ -14,4 +18,32 @@ const Container = styled.div`
   margin: 0px;
   padding: 0px;
   padding-bottom: 200px;
+`
+const Chose = styled.div`
+ background-color: #FFFFFF;
+  width: 100vw;
+  height: 110px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+    p{
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 24px;
+        color: #293845;
+    }
+`
+const Menu = styled.div`
+width: 100vw;
+height: 67px;
+display: flex;
+justify-content: center;
+background-color: #C3CFD9;
+align-items: center;
+    p {
+        color: #E8833A;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 34px;
+    }
 `
