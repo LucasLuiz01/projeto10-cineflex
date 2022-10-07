5f1e12790dc189c72340201ebf8d5becb119158c
Lucas Luiz <lucas.luiz10498@gmail.com>
2022-10-07 18:30:36 -0300

    versao com a pagina 2 funcionando

--- src/PageTwo.js
+++ src/PageTwo.js
@@ -1,32 +1,109 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+export default function PageTwo({ filmeEscolhido, setFilmeEscolhido }) {
+  const params = useParams();
+  useEffect(() => {
+    const Url = `https://mock-api.driven.com.br/api/v5/cineflex/movies/${params.id}/showtimes`;
+    const promisse = axios.get(Url);
+    promisse.then((movie) => {
+      setFilmeEscolhido(movie.data);
+    });
+    promisse.catch((erro) => {
+      console.log(erro.response.data);
+    });
+  }, []);
+  if (filmeEscolhido !== null) {
+    console.log(filmeEscolhido);
+    return (
+      <>
+        <Chose>
+          <p>Selecione o horário</p>
+        </Chose>
+        {filmeEscolhido.days.map((info) => {
+          return (
+            <>
+              <Dia>
+                {info.weekday} - {info.date}
+              </Dia>
+              <Horario>
+                {info.showtimes.map((inf) => (
+                  <button>{inf.name}</button>
+                ))}
+              </Horario>
+            </>
+          );
+        })}
+        <Footer><img src={filmeEscolhido.posterURL} /><p>{filmeEscolhido.title}</p></Footer>
+      </>
+    );
+  }
 
-export default function PageTwo(){
-    return(
-        <>
-        <Chose><p>Selecione o horário</p></Chose>
-        <Dia>Quinta-feira - 24/06/2021</Dia>
-        </>
-    )
+  return <></>;
 }
+
 const Dia = styled.p`
-font-family: 'Roboto', sans-serif;
-        font-weight: 400;
-        font-size: 20px;
-        color: #293845;
-        margin: auto;
-`
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  font-size: 20px;
+  color: #293845;
+  margin: auto;
+`;
+const Horario = styled.div`
+  display: flex;
+  justify-content: center;
+  button {
+    font-family: "Roboto", sans-serif;
+    background-color: #e8833a;
+    font-weight: 400;
+    border-radius: 3px;
+    width: 83px;
+    height: 43px;
+    color: white;
+    text-align: center;
+    text-decoration: none;
+    font-size: 16px;
+    margin-right: 20px;
+    margin-bottom: 23px;
+    margin-top: 23px;
+  }
+`;
 
 const Chose = styled.div`
- background-color: #FFFFFF;
+  background-color: #ffffff;
   width: 100vw;
   height: 110px;
   display: flex;
   align-items: center;
   justify-content: center;
-    p{
-        font-family: 'Roboto', sans-serif;
-        font-weight: 400;
-        font-size: 24px;
-        color: #293845;
-    }
-`
+  p {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 24px;
+    color: #293845;
+  }
+`;
+const Footer = styled.div`
+  background-color: #dfe6ed;
+  width: 100vw;
+  height: 117px;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  bottom: 0;
+  position: fixed;
+  border: 1px solid #9eadba;
+    img {
+        width: 48px;
+        height: 72px;
+        margin-left: 20px;
+  }
+    p {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 26px;
+    color: #293845;
+    margin-left: 20px;
+  }
+`;
--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
-import styled from "styled-components"
-import PageChoseMovie from "./PageChoseMovie"
-import React, { useEffect } from "react"
-import PageTwo from "./PageTwo"
+import styled from "styled-components";
+import PageChoseMovie from "./PageOne";
+import React from "react";
+import PageTwo from "./PageTwo";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-    
- 
-    return (
-        <Container >
-            <Menu><p>CINEFLIX</p></Menu>
-        <PageChoseMovie />
-        <PageTwo/>
-        </Container>
-    )
+  const [filmeEscolhido, setFilmeEscolhido] = React.useState(null);
+
+  return (
+    <BrowserRouter>
+      <Container>
+        <Menu>
+          <p>CINEFLIX</p>
+        </Menu>
+        <Routes>
+          <Route path="/" element={<PageChoseMovie />} />
+          <Route
+            path="/filme/:id"
+            element={
+              <PageTwo
+                setFilmeEscolhido={setFilmeEscolhido}
+                filmeEscolhido={filmeEscolhido}
+              />
+            }
+          />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  );
 }
 const Container = styled.div`
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   width: 100vw;
   min-height: 100vh;
   display: flex;
@@ -24,19 +39,19 @@ const Container = styled.div`
   margin: 0px;
   padding: 0px;
   padding-bottom: 200px;
-`
+`;
 
 const Menu = styled.div`
-width: 100vw;
-height: 67px;
-display: flex;
-justify-content: center;
-background-color: #C3CFD9;
-align-items: center;
-    p {
-        color: #E8833A;
-        font-family: 'Roboto', sans-serif;
-        font-weight: 400;
-        font-size: 34px;
-    }
-`
+  width: 100vw;
+  height: 67px;
+  display: flex;
+  justify-content: center;
+  background-color: #c3cfd9;
+  align-items: center;
+  p {
+    color: #e8833a;
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 34px;
+  }
+`;
